76224e1bc02119eac82925fb1c1f216a59481d62
XieWeijie <xieweijie9812@126.com>
2024-03-22 22:31:50 +0800

    update init arch script

--- scripts/init_arch.sh
+++ scripts/init_arch.sh
@@ -4,5 +4,19 @@
 # sed -i 's#Server = http://mirrors.163.com/archlinux/$repo/os/$arch#Server = http://mirrors.ustc.edu.cn/archlinux/$repo/os/$arch#g' /etc/pacman.d/mirrorlist
 
 # Install Packages
-pacman -S --noconfirm vim git wget curl ctags
+sudo pacman -S --noconfirm vim git wget curl ctags
+
+if [ -d $HOME/workspace ];then
+  mkdir $HOME/workspace/
+fi
+
+if [ -d $HOME/workspace/nerd-fonts ]; then
+  pushd $HOME/workspace/
+  git clone https://github.com/ryanoasis/nerd-fonts.git
+  pushd nerd-fonts
+  ./install.sh
+  popd
+  popd
+fi
+
 
